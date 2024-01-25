1688914e2142fe047e0a5625952f6ca974354ba0
w-carey <120460436+w-carey@users.noreply.github.com>
2024-01-25 13:31:55 +0000

    Create config font and colours.py

---
+++ guizero stuff/tkinter stuff/config font and colours.py
@@ -0,0 +1,12 @@
+keyforms = [WORKOUTGUIDES, TIMETABLE, HOMEMENU]
+_MENU_accountsettingsbox_.tk.config(bg="#BFBFBF")
+_MENU_accountsettings_.tk.config(bg="#BFBFBF", font=(defaultfont, 11))
+MENU_workoutguidesbutton.tk.config(bg="#BFBFBF", font=(defaultfont, 11), borderwidth=0, activebackground="#BFBFBF")
+MENU_timetableplannerbutton.tk.config(bg="#BFBFBF", font=(defaultfont, 11), borderwidth=0, activebackground="#BFBFBF")
+MENU_buypremiumbox.set_border(1, "#FFFF00")
+MENU_buypremiumbutton.tk.config(bg="#BFBFBF", font=(defaultfont, 11), borderwidth=0, activebackground="#BFBFBF")
+MENU_viewsetgoalsbutton.tk.config(bg="#D8D8D8", font=(defaultfont, 11), borderwidth=0, activebackground="#D8D8D8")
+MENU_viewsetgoalsbox.set_border(1, "#FFFF00")
+MENU_signoutbutton.tk.config(bg="#BFBFBF", font=(defaultfont, 11), borderwidth=0, activebackground="#BFBFBF")
+MENU_homemenubutton.tk.config(bg="#E6E1C1", fg="#4E4398", font=(defaultfont, 11), borderwidth=0, activebackground="#E6E1C1", activeforeground="#4E4398")
+#
