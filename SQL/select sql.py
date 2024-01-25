0a78341f53f520afd5d97e1e9028b47287fbfebd
w-carey <120460436+w-carey@users.noreply.github.com>
2024-01-25 13:20:59 +0000

    Update select sql.py

--- SQL/select sql.py
+++ SQL/select sql.py
@@ -1,2 +1,1 @@
-sqlExecutor(["INSERT INTO tokafitnessDB_user(user_ID, user_forename, user_surname, user_email, user_name, user_password, user_darkmode, user_font, user_premium, user_eventlog) VALUES(?,?,?,?,?,?,?,?,?,?)",
-(None, forenameTB.value, surnameTB.value, emailTB.value, usernameTB.value, passwordTB.value, False, "Calibri", 0, "account created on '"+str(datetime.datetime.now())+"'")])
+sqlExecutor(["SELECT user_eventlog FROM tokafitnessDB_user WHERE user_ID == ?", (currentlyloggedinto,)])[0][0]
