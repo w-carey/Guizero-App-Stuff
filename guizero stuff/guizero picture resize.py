478382b0d66e28303be5a295dbb8502c45c10fc4
w-carey <120460436+w-carey@users.noreply.github.com>
2024-01-25 13:29:34 +0000

    Create guizero picture resize.py

---
+++ guizero stuff/guizero picture resize.py
@@ -0,0 +1,2 @@
+LOGIN_newlogoimage			= ImageTk.PhotoImage((Image.open("logo.png")).resize((117, 31), Image.Resampling.LANCZOS))
+LOGIN_bottomlogo				= Picture(LOGIN_main, image=LOGIN_newlogoimage, align="bottom")
