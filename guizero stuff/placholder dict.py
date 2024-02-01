c545d548496b9c5c360d215546c8dafc8dd0792a
w-carey <120460436+w-carey@users.noreply.github.com>
2024-02-01 11:13:44 +0000

    Create placholder dict.py

---
+++ guizero stuff/placholder dict.py
@@ -0,0 +1,17 @@
+placeholdertext = {
+	"FORENAME"		: "FORENAME...",
+	"SURNAME"		: "SURNAME...",
+	"USERNAME"		: "USERNAME...",
+	"PASSWORD"		: "PASSWORD...",
+	"CONFIRMP"		: "CONFIRM PASSWORD...",
+	"CURRENTP"  	: "CURRENT PASSWORD...",
+	"NEWP"			: "NEW PASSWORD...",
+	"CONFIRMNEWP"	: "CONFIRM NEW PASSWORD...",
+	"EMAIL"			: "EMAIL...",
+	"CONFIRME"		: "CONFIRM EMAIL...",
+	"NEWE"			: "NEW EMAIL...",
+	"WEIGHT"		: "WEIGHT (kg)...",
+	"WRITEPLAN"		: "Write plan here...",
+	"TITLE"			: "Title...",
+	"FONT"			: "Font search..."
+}
