733b18c6d27dbd5aa76a3cd6c6f37b4195babca6
w-carey <120460436+w-carey@users.noreply.github.com>
2024-02-28 15:47:45 +0000

    Create top5.py

---
+++ testingclassstuff/top5.py
@@ -0,0 +1,28 @@
+
+
+
+####################################### frontend stuff below		
+App		    = gz.App
+Picture		= gz.Picture
+Text		= gz.Text
+PushButton	= gz.PushButton
+CheckBox	= gz.CheckBox
+ListBox     = gz.ListBox
+Combo		= gz.Combo
+class TextBox(gz.TextBox):
+	def __init__(self, master, placeholder=None, *arguments, **keywords):
+		gz.TextBox.__init__(self, master, *arguments, **keywords)
+		self.placeholder = placeholder
+		if placeholder:
+			placeholderfunc(self, "reset", False)
+		####
+		alltextboxes.append(self)
+	####
+####
+class Box(gz.Box):
+	def __init__(self, master, *arguments, **keywords):
+		gz.Box.__init__(self, master, *arguments, **keywords)
+		allboxesWH.append([self, master])
+	####
+####		
+#
