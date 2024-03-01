38e172f2d4751100779cc9f12a5d1827e91dcf9a
w-carey <120460436+w-carey@users.noreply.github.com>
2024-03-01 12:02:56 +0000

    Create formattingdates1.py

---
+++ datetime stuff/formattingdates1.py
@@ -0,0 +1,9 @@
+from datetime import date
+date.fromisoformat('2019-12-04')
+#datetime.date(2019, 12, 4)
+
+date.fromisoformat('20191204')
+#datetime.date(2019, 12, 4)
+
+date.fromisoformat('2021-W01-1')
+#datetime.date(2021, 1, 4)
