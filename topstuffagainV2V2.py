e0f4c104e7ff3c77c55d09463c3165801fd46726
w-carey <120460436+w-carey@users.noreply.github.com>
2024-03-01 10:45:34 +0000

    Create topstuffagainV2V2.py

---
+++ topstuffagainV2V2.py
@@ -0,0 +1,6 @@
+from math import e
+import tkinter as tk
+import guizero as gz
+import sqlite3
+databaseFile = "bnb.db"
+allboxesWH,allForms,alltextboxes,placeholdertext,currentlyloggedinto,minimenu = [],[],[],{},-1,[]
