5459e3962936f0a4b8a6ddffc7c26884f99c76cc
w-carey <120460436+w-carey@users.noreply.github.com>
2024-03-01 10:50:20 +0000

    Create formattingaxis2.py

---
+++ matplotlib/formattingaxis2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.spines['bottom'].set_color('blue')
+ax.spines['left'].set_color('red')
+ax.spines['left'].set_linewidth(2)
+ax.spines['right'].set_color(None)
+ax.spines['top'].set_color(None)
+ax.plot([1,2,3,4,5])
+plt.show()
