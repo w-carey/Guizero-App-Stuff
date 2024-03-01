73eb763837cbcaeb2b765a5f387bde2bc3808f67
w-carey <120460436+w-carey@users.noreply.github.com>
2024-03-01 10:51:52 +0000

    Create multiplots3.py

---
+++ matplotlib/multiplots3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+x = np.arange(0, math.pi*2, 0.05)
+fig=plt.figure()
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
+axes2 = fig.add_axes([0.55, 0.55, 0.3, 0.3]) # inset axes
+y = np.sin(x)
+axes1.plot(x, y, 'b')
+axes2.plot(x,np.cos(x),'r')
+axes1.set_title('sine')
+axes2.set_title("cosine")
+plt.show()
